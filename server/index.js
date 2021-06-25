d6299e2167ac27d7f43bdb2716b5d19793c31f7e
Bhanu_Nelluri <bhanunelluri0829@gmail.com>
2021-06-25 17:09:03 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 
-app.use(express.static(path.join(__dirname, '/public')));
+// app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
@@ -48,9 +48,9 @@ app.use('/users', userRoutes);
 
 const port = process.env.PORT || 4001;
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/public/index.html'));
+// });
 
 app.listen(port, () => {
     console.log(`Welcome to server : ${port}`);
